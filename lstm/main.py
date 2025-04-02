b7ff020f9209c87eff4be0e4052b2a15e58f8ab1
Artem <artem.riabinin@kaust.edu.sa>
2025-04-02 20:04:03 +0300

    no message

--- lstm/main.py
+++ lstm/main.py
@@ -290,21 +290,9 @@ def train():
             optimizer.step()
             optimizer.zero_grad()
             print(f"| epoch {epoch:3d} | total loss {total_loss:.2f} |")
-            total_loss = 0
 
         optimizer.param_groups[0]['lr'] = lr2
         
-        if batch % args.smooth_log_interval == 0 and batch > 0:
-            iterationlogger.write_row(eval_smooth(prev_model, model))
-        
-        if batch % args.log_interval == 0 and batch > 0:
-            cur_loss = total_loss.item() / args.log_interval
-            elapsed = time.time() - start_time
-            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:05.5f} | ms/batch {:5.2f} | '
-                    'loss {:5.2f} | ppl {:8.2f} | bpc {:8.3f}'.format(
-                epoch, batch, len(train_data) // args.bptt, optimizer.param_groups[0]['lr'],
-                elapsed * 1000 / args.log_interval, cur_loss, math.exp(cur_loss), cur_loss / math.log(2)))
-            start_time = time.time()
         ###
         batch += 1
         i += seq_len

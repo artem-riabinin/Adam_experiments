67bf1fc2c3538d4dd7ebc91a98326d666d2c3fa7
Artem <artem.riabinin@kaust.edu.sa>
2025-04-13 13:12:10 +0300

    no message

--- ResNet20/train_fullbatch_beta1_0_beta2_0.999_lr_0.001.py
+++ ResNet20/train_fullbatch_beta1_0_beta2_0.999_lr_0.001.py
@@ -8,8 +8,8 @@ import torchvision.transforms as transforms
 
 # wandb logging
 wandb_log = True 
-wandb_project = 'resnet20_experiments'
-wandb_run_name = 'fullbatch_beta1_0.1_beta2_0.5_lr_0.001'
+wandb_project = 'resnet20_exp'
+wandb_run_name = 'fullbatch_beta1_0_beta2_0.999_lr_0.001'
 
 
 
@@ -21,8 +21,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Hyper-parameters
 num_epochs = 500
 learning_rate = 0.001
-beta1 = 0.1
-beta2 = 0.5
+beta1 = 0
+beta2 = 0.999
 betas = (beta1, beta2)
 mini_batch_size = 10000
 accumulation_steps = 5
